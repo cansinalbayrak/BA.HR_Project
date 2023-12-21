b19d571c39657ca46add8a4a369f7990d9e9a17b
Berk Alp Direm <berkalpdirem@hotmail.com>
2023-12-21 14:20:00 +0300

    ImageUpload Helper Method Complated

---
+++ BA.HR_Project.WEB/HelperMethods/Helper.cs
@@ -0,0 +1,17 @@
+﻿namespace BA.HR_Project.WEB.HelperMethods
+{
+    public class Helper
+    {
+        public string SaveImageFile(IFormFile imageFile)
+        {
+            var fileName = Guid.NewGuid().ToString() + "_" + imageFile.FileName;
+            var imagePath = Path.Combine("wwwroot", "images", fileName);
+
+            using (var stream = new FileStream(imagePath, FileMode.Create))
+            {
+                imageFile.CopyTo(stream);
+            }
+            return "/images/" + fileName;
+        }
+    }
+}
