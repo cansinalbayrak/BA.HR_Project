3387a1bed9b6b3100eff427d16b9a89b9d6fc26e
OzanUlus <ilkerozanulus@gmail.com>
2023-12-21 16:17:44 +0300

    fixed somethings

--- BA.HR_Project.Infrastructure/Extension/InfrastructureDependencies.cs
+++ BA.HR_Project.Infrastructure/Extension/InfrastructureDependencies.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using BA.HR_Project.Infrasturucture.Managers.Concrate;
+using BA.HR_Project.Infrasturucture.Services.Concrate;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BA.HR_Project.Infrastructure.Extension
 {
-    public class InfrastructureDependencies
+    public static class InfrastructureDependencies
     {
+        public static  void AddInfrastructureDependencies(this IServiceCollection services) 
+        {
+            services.AddScoped<IAdressService, AdressManager>();
+            services.AddScoped<IAppRoleService, AppRoleManager>();
+            services.AddScoped<IAppUserService, AppUserManager>();
+            services.AddScoped<ICompanyService, CompanyManager>();
+            services.AddScoped<IDepartmentService, DepartmentManager>();
+        }
     }
 }
