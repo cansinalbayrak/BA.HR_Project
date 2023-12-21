b719eb9c1fdfb3149edae092d2355c4e841ce870
Cansın Albayrak <cano.omur95@gmail.com>
2023-12-21 11:24:55 +0300

    summary info action(index) moved from home to employee controller

--- BA.HR_Project.WEB/Controllers/EmployeeController.cs
+++ BA.HR_Project.WEB/Controllers/EmployeeController.cs
@@ -26,7 +26,25 @@ namespace BA.HR_Project.WEB.Controllers
             _adressManager = adressManager;
             _mapper = mapper;
         }
-        
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var GetuserdtoAction = await _appUserManager.Get(true, x => x.Id == userId);
+            var userdto = GetuserdtoAction.Context;
+
+            var departmentId = userdto.DepartmentId;
+            var companyId = userdto.CompanyId;
+            var company = await _companyManager.Get(true, x => x.Id == companyId);
+            var department = await _departmentManager.Get(true, x => x.Id == departmentId);
+            var adress = await _adressManager.Get(true, x => x.Id == userdto.AdressId);
+
+            var userViewModels = _mapper.Map<ListSummarInfoViewModel>(userdto);
+            ViewBag.Department = department.Context;
+            ViewBag.Company = company.Context;
+            ViewBag.Adress = adress.Context;
+            return View(userViewModels);
+        }
+
         public async Task<IActionResult> Detail()
         {
             var userId = _userManager.GetUserId(User);
