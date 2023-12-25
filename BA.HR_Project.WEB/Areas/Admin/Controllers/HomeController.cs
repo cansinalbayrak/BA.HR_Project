b609b625cf9011cc3512e7ed7ef3e2de37f26c92
OzanUlus <ilkerozanulus@gmail.com>
2023-12-25 10:08:31 +0300

    created admin area

---
+++ BA.HR_Project.WEB/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BA.HR_Project.WEB.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
