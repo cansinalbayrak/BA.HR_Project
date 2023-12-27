b903bf2b71d0da388576ffc68a42d59e49ab3aaf
Berk Alp Direm <berkalpdirem@hotmail.com>
2023-12-27 14:39:46 +0300

    ImageLayout started for Admin pages backgound ımage complated

--- BA.HR_Project.WEB/Areas/Admin/Controllers/AdminHomeController.cs
+++ BA.HR_Project.WEB/Areas/Admin/Controllers/AdminHomeController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using BA.HR_Project.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace BA.HR_Project.WEB.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize]
     public class AdminHomeController : Controller
     {
-        [HttpGet("/Admin/Home/Index")]
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+        public AdminHomeController(UserManager<AppUser> userManager,IMapper mapper)
         {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var GetUserAction = await _userManager.GetUserAsync(User);
+
+            if (GetUserAction !=null) 
+            {
+                var Welcome = GetUserAction.Name + " " + GetUserAction.Surname;
+                ViewBag.Welcome = Welcome;
+                return View();
+            }
             return View();
         }
     }
