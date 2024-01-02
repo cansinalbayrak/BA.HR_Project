680430ccd78de96b61f8afef2bfc8aea7feb5054
Cansın Albayrak <cano.omur95@gmail.com>
2024-01-02 10:34:10 +0300

    added currency type enum

---
+++ BA.HR_Project.Domain/Enums/Currency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA.HR_Project.Domain.Enums
+{
+    public enum Currency
+    {
+        TRY=1,
+        USD,
+        EUR
+    }
+}
