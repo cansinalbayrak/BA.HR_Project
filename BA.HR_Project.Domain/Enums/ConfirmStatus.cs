84fe089406de541b8617ca51552a4cded9852cbd
Cansın Albayrak <cano.omur95@gmail.com>
2024-01-02 10:18:00 +0300

    added status enum

---
+++ BA.HR_Project.Domain/Enums/ConfirmStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA.HR_Project.Domain.Enums
+{
+    public enum ConfirmStatus
+    {
+        Waiting=1,
+        Approved,
+        Denied
+    }
+}
