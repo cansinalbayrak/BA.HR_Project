aa4a97d1956f7d073c529c981475966447e75c37
sergen aktaş <srgn_akts@hotmail.com>
2024-01-03 21:17:11 +0300

    added

--- BA.HR_Project.WEB/Models/DayOffViewModel.cs
+++ BA.HR_Project.WEB/Models/DayOffViewModel.cs
@@ -5,18 +5,26 @@ namespace BA.HR_Project.WEB.Models
 {
     public class DayOffViewModel
     {
+        public DayOffViewModel()
+        {
+            Id = Guid.NewGuid().ToString();
+            // Diğer başlangıç değerlerini de burada ayarlayabilirsiniz.
+            RequestDate = DateTime.Now;
+            ConfirmStatus = ConfirmStatus.Waiting;
+        }
+
         public string Id { get; set; }
         public string? Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime FinishDate { get; set; }
         public DateTime? RequestDate { get; set; }
         public float? DayCount { get; set; }
-        public ConfirmStatus? ConfirmStatus { get; set; }
+        public ConfirmStatus ConfirmStatus { get; set; }
         public Gender Gender { get; set; }
         public DateTime? ResponseDate { get; set; }
         public string AppUserId { get; set; }
         public DayOffType DayOffType { get; set; }
 
-        public AppUserViewModel AppUser { get; set; }
+        public AppUserViewModel AppUser { get; set; } = new AppUserViewModel();
     }
 }
--- BA.HR_Project.WEB/Program.cs
+++ BA.HR_Project.WEB/Program.cs
@@ -69,20 +69,20 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseExceptionHandler(errorApp =>
-{
-    errorApp.Run(async context =>
-    {
-        context.Response.StatusCode = 500;
-        context.Response.ContentType = "text/html";
+//app.UseExceptionHandler(errorApp =>
+//{
+//    errorApp.Run(async context =>
+//    {
+//        context.Response.StatusCode = 500;
+//        context.Response.ContentType = "text/html";
 
-        await context.Response.WriteAsync("<script>window.location='/Error/Warning';</script>");
-    });
-});
+//        await context.Response.WriteAsync("<script>window.location='/Error/Warning';</script>");
+//    });
+//});
 
-app.UseStatusCodePagesWithRedirects("/Error/Warning/{0}");
+//app.UseStatusCodePagesWithRedirects("/Error/Warning/{0}");
 
-app.UseMiddleware<ExceptionHandlingMiddleware>();
+//app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseStaticFiles();
 
 app.UseRouting();
