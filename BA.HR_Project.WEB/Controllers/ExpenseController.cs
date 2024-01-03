9e065fa199092d770fd5103e6ab490795e72e837
Berk Alp Direm <berkalpdirem@hotmail.com>
2024-01-03 22:59:26 +0300

    Expense edited

--- BA.HR_Project.WEB/Controllers/ExpenseController.cs
+++ BA.HR_Project.WEB/Controllers/ExpenseController.cs
@@ -31,42 +31,42 @@ namespace BA.HR_Project.WEB.Controllers
             return View();
         }
         [HttpGet]
-        public async Task<IActionResult> RequestExpense() 
+        public async Task<IActionResult> RequestExpense()
         {
             List<ExpenseType> allExpenseTypes = _expenseTypeService.GetAll();
-            List<string> expenseNames  = new List<string>();
+            List<string> expenseNames = new List<string>();
             for (int i = 0; i < allExpenseTypes.Count; i++)
             {
                 expenseNames.Add(allExpenseTypes[i].ExpenseName);
             }
-            
+
             ViewBag.ExpenseTypes = expenseNames;
-            
+
             return View();
 
-            
+
         }
         [HttpPost]
         public async Task<IActionResult> RequestExpense(ExpenseViewModel model)
         {
             var userId = _userManager.GetUserId(User);
             model.AppUserId = userId;
-             
-              var ExpenseDto = _mapper.Map<ExpenseDto>(model);
+
+            var ExpenseDto = _mapper.Map<ExpenseDto>(model);
             var ExpenseAction = await _expsenseService.RequestExpense(ExpenseDto);
 
             //var ExpenseAction = await _expsenseService.Insert(ExpenseDto);
             if (ExpenseAction.IsSuccess)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", "Home");
             }
 
 
 
 
-            return View (model);
+            return View(model);
         }
 
-        
+
     }
 }
