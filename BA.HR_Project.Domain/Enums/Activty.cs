775d826cecf8d8ca27c57c90f5e4303a8cef2fd1
Berk Alp Direm <berkalpdirem@hotmail.com>
2024-01-08 16:07:18 +0300

    Company Entity Edited, Controller Created and AddCompany action  is done, alson add company views is finished

---
+++ BA.HR_Project.Domain/Enums/Activty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA.HR_Project.Domain.Enums
+{
+    public enum Activty
+    {
+        Pasive = 0,
+        Active
+    }
+}
--- BA.HR_Project.WEB/Models/CompanyViewModel.cs
+++ BA.HR_Project.WEB/Models/CompanyViewModel.cs
@@ -1,13 +1,26 @@
 ﻿using BA.HR_Project.Application.DTOs;
+using BA.HR_Project.Domain.Enums;
 
 namespace BA.HR_Project.WEB.Models
 {
     public class CompanyViewModel
     {
         public string Id { get; set; }
+        public CompanyTitle CompanyTitleEnum { get; set; }
         public string Name { get; set; }
         public string LogoPath { get; set; }
 
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+        public string Mail { get; set; }
+        public int EmployeeCount { get; set; }
+        public string MersisNo { get; set; }
+        public string TaxNo { get; set; }
+        public string TaxOffice { get; set; }
+        public DateTime StartUpDate { get; set; }
+        public DateTime ContractStartDate { get; set; }
+        public DateTime ContractEndDate { get; set; }
+        public Activty ActivtyEnum { get; set; }
         public List<AppUserViewModel> AppUsers { get; set; }
     }
 }
