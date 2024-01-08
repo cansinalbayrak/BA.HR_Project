8678e1b67420c71f795d2abf196f1c15e487aadd
OzanUlus <ilkerozanulus@gmail.com>
2024-01-08 23:04:07 +0300

    created two methods

--- BA.HR_Project.Infrastructure/Managers/Concrate/CompanyManager.cs
+++ BA.HR_Project.Infrastructure/Managers/Concrate/CompanyManager.cs
@@ -9,14 +9,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BA.HR_Project.Persistance.Context;
 
 namespace BA.HR_Project.Infrasturucture.Managers.Concrate
 {
     public class CompanyManager : BaseManager<Company, CompanyDto>, ICompanyService
     {
-        public CompanyManager(IMapper mapper, IUow uow) : base(mapper, uow)
+        private readonly AppDbContext _appDbContext;
+        public CompanyManager(IMapper mapper, IUow uow, AppDbContext appDbContext) : base(mapper, uow)
         {
-
+            _appDbContext = appDbContext;
         }
+
+        
+
+        public List<CompanyCustom> GetAllCompanyCustomColumn()
+        {
+            return _appDbContext.Companies
+                        .AsEnumerable() 
+                        .Select(c => new CompanyCustom { Id = c.Id.ToString(), CompanyName = c.Name })
+                        .ToList();
+        }
+
+        
+    }
+    public class CompanyCustom
+    {
+ 
+        public string Id { get; set; }
+        public string CompanyName { get; set; }
+   
     }
 }
