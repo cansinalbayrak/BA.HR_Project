3d19b95019905314f1293a8bd98bd9097bb114c4
sergen aktaş <srgn_akts@hotmail.com>
2024-01-10 01:42:04 +0300

    Added Mersisno and text no validator and logo validator

--- BA.HR_Project.WEB/ModelValidators/CompanyViewModelValidator.cs
+++ BA.HR_Project.WEB/ModelValidators/CompanyViewModelValidator.cs
@@ -9,20 +9,49 @@ namespace BA.HR_Project.WEB.ModelValidators
         public CompanyViewModelValidator()
         {
 			RuleFor(x => x.Name).NotEmpty().WithMessage("Name must be provided");
-			RuleFor(x => x.LogoPath).NotEmpty().WithMessage("LogoPath must be provided");
+            RuleFor(x => x.Photo)
+             .Must((model, file) => file != null || model.UseExistingPhoto)
+             .WithMessage("File must be provided or Choose This Photo")
+             .When(x => !x.UseExistingPhoto);
 
-			RuleFor(x => x.Name).MinimumLength(3).WithMessage("Name must be at least 3 characters long");
+            RuleFor(x => x.Photo)
+                .Must(file => file == null || IsAllowedImageFile(file))
+                .WithMessage("Invalid file format. Please choose a valid image file (jpeg or jpg).")
+                .When(x => !x.UseExistingPhoto);
+
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Name must be at least 3 characters long");
             RuleFor(x => x.Phone).NotEmpty().WithMessage("Phone must be provided").Matches("^[0-9]{10}$").WithMessage("Phone must be 10 digits long and contain only numbers");
             RuleFor(x => x.TaxNo).NotEmpty().WithMessage("Phone must be provided").Matches("^[0-9]{10}$").WithMessage("Text no must be 10 digits long and contain only numbers");
             RuleFor(x => x.Mail).NotEmpty().NotEmpty().WithMessage("Phone must be provided").EmailAddress().WithMessage("Invalid email format");
-            RuleFor(x => x.StartUpDate).NotEmpty().WithMessage("StartUpDate must be provided");
-            RuleFor(x => x.ContractStartDate).NotEmpty().WithMessage("ContractStartDate must be provided")
-                .LessThan(x => x.ContractEndDate).WithMessage("ContractStartDate must be before ContractEndDate");
+            RuleFor(x => x.StartUpDate)
+                .NotEmpty().WithMessage("StartUpDate must be provided")
+                .Must(BeValidDate).WithMessage("StartUpDate must be before ContractStartDate and before current date");
+
+            RuleFor(x => x.ContractStartDate)
+                .NotEmpty().WithMessage("ContractStartDate must be provided")
+                .Must((viewModel, contractStartDate) => BeValidDate(contractStartDate) && contractStartDate < viewModel.ContractEndDate && contractStartDate > viewModel.StartUpDate)
+                .WithMessage("ContractStartDate must be between StartUpDate and before ContractEndDate");
+
+            RuleFor(x => x.ContractEndDate)
+                .NotEmpty().WithMessage("ContractEndDate must be provided")
+                .GreaterThan(x => x.ContractStartDate).WithMessage("ContractEndDate must be after ContractStartDate");
 
             RuleFor(x => x.MersisNo)
-                   .NotEmpty().WithMessage("MersisNo must be provided")
-                   .Must((viewModel, mersisNo) => ValidateMersisNo(viewModel.CompanyTitleEnum, mersisNo))
-                   .WithMessage("Invalid MersisNo for the selected CompanyTitle");
+                .NotEmpty().WithMessage("MersisNo must be provided")
+                .Must((viewModel, mersisNo) => ValidateMersisNo(viewModel.CompanyTitleEnum, mersisNo))
+                .WithMessage("Invalid MersisNo for the selected CompanyTitle");
+
+          
+        }
+        private bool IsAllowedImageFile(IFormFile file)
+        {
+            var allowedExtensions = new[] { ".jpg", ".jpeg" };
+            var extension = Path.GetExtension(file.FileName)?.ToLowerInvariant();
+            return !string.IsNullOrEmpty(extension) && allowedExtensions.Contains(extension);
+        }
+        private bool BeValidDate(DateTime date)
+        {
+            return date < DateTime.Now && date < DateTime.Now.Date.AddHours(23).AddMinutes(59).AddSeconds(59);
         }
 
         private bool ValidateMersisNo(CompanyTitle companyTitle, string mersisNo)
