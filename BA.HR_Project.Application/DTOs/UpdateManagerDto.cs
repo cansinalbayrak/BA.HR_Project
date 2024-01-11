de442c40254875843eb5a1a6f9fd375629dd70d4
OzanUlus <ilkerozanulus@gmail.com>
2024-01-11 09:42:21 +0300

    done

--- BA.HR_Project.Application/DTOs/UpdateManagerDto.cs
+++ BA.HR_Project.Application/DTOs/UpdateManagerDto.cs
@@ -16,6 +16,18 @@ namespace BA.HR_Project.Application.DTOs
         public string? SecondSurname { get; set; }
         public string? PhotoPath { get; set; }
         public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? Salary { get; set; }
+        public bool IsActive { get; set; }
+        public string? BirthPlace { get; set; }
+        public bool IsTurkishCitizen { get; set; }
+        public string? IdentityNumber { get; set; }
+
+        public string? PassportNumber { get; set; }
+        public string Adress { get; set; }
 
         public string CompanyId { get; set; }
         public string DepartmentId { get; set; }
--- BA.HR_Project.WEB/Areas/Manager/Models/UpdateManagerViewModelcs.cs
+++ BA.HR_Project.WEB/Areas/Manager/Models/UpdateManagerViewModelcs.cs
@@ -11,8 +11,18 @@
         public IFormFile Photo { get; set; }
         public string? PhotoPath { get; set; }
         public string PhoneNumber { get; set; }
-        public bool UseExistingPhoto { get; set; }
+        public DateTime BirthDate { get; set; }
 
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? Salary { get; set; }
+        public bool IsActive { get; set; }
+        public string? BirthPlace { get; set; }
+        public bool IsTurkishCitizen { get; set; }
+        public string? IdentityNumber { get; set; }
+
+        public string? PassportNumber { get; set; }
+        public string Adress { get; set; }
         public string CompanyId { get; set; }
         public string DepartmentId { get; set; }
        
